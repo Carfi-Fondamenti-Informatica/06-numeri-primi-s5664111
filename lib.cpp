0d475e2492208388d95b13dfa8fc14afaeb3dd1b
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-03 11:44:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,13 +3,15 @@ bool primi(int n){
   int div=1;
   int conta=0;
   while(conta<=1 && div<=n/2){
-    if(n%div==0){
+    if(n%div=0){
       conta++;
     }
+    else{
     div++;
   }
   if(conta==1){
     return true;
-  }else{
+  }
+   else{
     return false;
   }
